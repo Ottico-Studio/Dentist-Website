6e5e280356085593e235ed45153eadd5484efcac
Ottico-Studio <ottico@mailfence.com>
2024-02-04 20:12:10 -0500

    First upload

---
+++ assets/Swiper-Script.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper(".team-slider",{
+
+  loop:true,
+  grabCursor:true,
+  spaceBetween:20,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    991: {
+      slidesPerView: 3,
+    },
+  },
+
+});
---
+++ index.php
@@ -0,0 +1,424 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title> Abundant Chiropractic </title>
+    <link rel="stylesheet" href="assets/AChiro.css">
+
+    <!-- FontAwesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
+    <!-- Swiper CSS -->
+    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css">
+
+
+  </head>
+  <body>
+
+
+
+    <!-- Header -->
+    <header class="header">
+
+      <a href="#" class="logo"> <i class="fas fa-tooth"></i> Dentist </a>
+      <nav class="navbar">
+        <a href="#"> Home </a>
+        <a href="#"> About </a>
+        <a href="#"> Services </a>
+        <a href="#"> Team </a>
+        <a href="#"> Pricing </a>
+        <a href="#"> Blog </a>
+      </nav>
+
+      <a href="#" class="btn"> Make Appointment </a>
+      <div class="fas fa-bars" id="menu-btn"></div>
+
+    </header>
+
+
+
+    <!-- Home -->
+    <section class="home" id="home">
+
+      <div class="content">
+        <h3> Make your smile shine </h3>
+        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+        <a href="#" class="btn"> Make Appointment </a>
+      </div>
+
+    </section>
+
+
+
+
+
+    <!-- About -->
+    <section class="about" id="about">
+
+      <h1 class="heading"> About Us </h1>
+
+      <div class="row">
+        <div class="image">
+          <img src="assets/images/about.webp" alt="">
+        </div>
+
+        <div class="content">
+          <h3> our Clinic is made for you to be smiling all the time </h3>
+          <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor inci. Duis aute irure dolor in rem dolore eu  nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+          <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incidicupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+          <a href="#" class="btn"> Read More </a>
+        </div>
+      </div>
+
+    </section>
+
+
+
+
+    <!-- Services -->
+    <section class="services" id="services">
+
+      <h1 class="heading"> Our Services </h1>
+
+      <div class="box-container">
+
+        <div class="box">
+          <img src="assets/images/services-1.webp" alt="">
+          <h3> Online Schedule </h3>
+          <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sunt in culpa qui officia deserunt mollit anim </p>
+        </div>
+
+        <div class="box">
+          <img src="assets/images/services-2.webp" alt="">
+          <h3> Cosmetic Feeling </h3>
+          <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sunt in culpa qui officia deserunt mollit anim </p>
+        </div>
+
+        <div class="box">
+          <img src="assets/images/services-3.webp" alt="">
+          <h3> Oral Hygiene </h3>
+          <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sunt in culpa qui officia deserunt mollit anim </p>
+        </div>
+
+      </div>
+
+    </section>
+    <!-- END Services -->
+
+
+
+
+    <!-- Team -->
+    <section class="team" id="team">
+
+      <h1 class="heading"> Our Team </h1>
+
+      <div class="swiper team-slider">
+        <div class="swiper-wrapper">
+
+          <!-- Slide 1 -->
+          <div class="swiper-slide slide">
+            <div class="image">
+              <img src="assets/images/team-1.jpg" alt="">
+              <div class="share">
+                <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-instagram"></a>
+                <a href="#" class="fab fa-linkedin"></a>
+              </div>
+            </div>
+
+            <div class="content">
+              <h3> Camille Plaza </h3>
+              <span> Optometrist </span>
+            </div>
+          </div>
+
+          <!-- Slide 2 -->
+          <div class="swiper-slide slide">
+            <div class="image">
+              <img src="assets/images/team-2.jpg" alt="">
+              <div class="share">
+                <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-instagram"></a>
+                <a href="#" class="fab fa-linkedin"></a>
+              </div>
+            </div>
+
+            <div class="content">
+              <h3> John Lacy </h3>
+              <span> General Surgeon </span>
+            </div>
+          </div>
+
+          <!-- Slide 3 -->
+          <div class="swiper-slide slide">
+            <div class="image">
+              <img src="assets/images/team-3.jpg" alt="">
+              <div class="share">
+                <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-instagram"></a>
+                <a href="#" class="fab fa-linkedin"></a>
+              </div>
+            </div>
+
+            <div class="content">
+              <h3> Alex Boyd </h3>
+              <span> Oncologist </span>
+            </div>
+          </div>
+
+          <!-- Slide 4 -->
+          <div class="swiper-slide slide">
+            <div class="image">
+              <img src="assets/images/team-4.jpg" alt="">
+              <div class="share">
+                <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-instagram"></a>
+                <a href="#" class="fab fa-linkedin"></a>
+              </div>
+            </div>
+
+            <div class="content">
+              <h3> Jake Gudger </h3>
+              <span> Psychology </span>
+            </div>
+          </div>
+
+        </div> <!-- END Swiper-Wrapper -->
+      </div>
+
+    </section>
+    <!-- END Team -->
+
+
+
+
+
+    <!-- Pricing -->
+    <section class="pricing" id="pricing">
+
+      <h1 class="heading"> Pricing Plans </h1>
+
+      <div class="box-container">
+        <!-- Plan 1 -->
+        <div class="box">
+          <h3 class="title"> Basic </h3>
+          <div class="price">
+            <span class="currency">$</span>
+            <span class="amount">18</span>
+            <span class="duration">/year</span>
+          </div>
+
+          <ul>
+            <li> <i class="fas fa-check"></i> Alignment Specialist </li>
+            <li> <i class="fas fa-check"></i> Cavity Inspection </li>
+            <li> <i class="fas fa-check"></i> Cosmetic Dentistry  </li>
+            <li> <i class="fas fa-check"></i> Oral Hygiene Experts  </li>
+          </ul>
+          <a href="#" class="btn"> read more </a>
+        </div>
+
+        <!-- Plan 2 -->
+        <div class="box active">
+          <h3 class="title"> Standard </h3>
+          <div class="price">
+            <span class="currency">$</span>
+            <span class="amount">30</span>
+            <span class="duration">/year</span>
+          </div>
+
+          <ul>
+            <li> <i class="fas fa-check"></i> Alignment Specialist </li>
+            <li> <i class="fas fa-check"></i> Cavity Inspection </li>
+            <li> <i class="fas fa-check"></i> Cosmetic Dentistry  </li>
+            <li> <i class="fas fa-check"></i> Oral Hygiene Experts  </li>
+          </ul>
+          <a href="#" class="btn"> read more </a>
+        </div>
+
+        <!-- Plan 3 -->
+        <div class="box">
+          <h3 class="title"> Premium </h3>
+          <div class="price">
+            <span class="currency">$</span>
+            <span class="amount">50</span>
+            <span class="duration">/year</span>
+          </div>
+
+          <ul>
+            <li> <i class="fas fa-check"></i> Alignment Specialist </li>
+            <li> <i class="fas fa-check"></i> Cavity Inspection </li>
+            <li> <i class="fas fa-check"></i> Cosmetic Dentistry  </li>
+            <li> <i class="fas fa-check"></i> Oral Hygiene Experts  </li>
+          </ul>
+          <a href="#" class="btn"> read more </a>
+        </div>
+
+      </div>
+    </section>
+    <!-- END Pricing -->
+
+
+
+
+
+    <!-- Blog -->
+    <section class="blog" id="blog">
+
+      <h1 class="heading"> Our Blogs </h1>
+
+      <div class="box-container">
+
+        <!-- Blog Post 1 -->
+        <div class="box">
+          <div class="image">
+            <img src="assets/images/blog-1.jpg" alt="">
+          </div>
+
+          <div class="content">
+            <a href="#" class="link"> Disease </a>
+            <div class="icons">
+              <a href="#"> <i class="fas fa-calendar"></i> 04 Sept, 2022 </a>
+              <a href="#"> <i class="fas fa-calender"></i> by admin </a>
+            </div>
+
+            <h3> Most Common Problems </h3>
+            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. </p>
+            <a href="#" class="btn"> read more </a>
+          </div>
+        </div>
+
+
+        <!-- Blog Post 2 -->
+        <div class="box">
+          <div class="image">
+            <img src="assets/images/blog-2.jpg" alt="">
+          </div>
+
+          <div class="content">
+            <a href="#" class="link"> Disease </a>
+            <div class="icons">
+              <a href="#"> <i class="fas fa-calendar"></i> 04 Sept, 2022 </a>
+              <a href="#"> <i class="fas fa-calender"></i> by admin </a>
+            </div>
+
+            <h3> Most Common Problems </h3>
+            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. </p>
+            <a href="#" class="btn"> read more </a>
+          </div>
+        </div>
+
+
+        <!-- Blog Post 3 -->
+        <div class="box">
+          <div class="image">
+            <img src="assets/images/blog-3.jpg" alt="">
+          </div>
+
+          <div class="content">
+            <a href="#" class="link"> Disease </a>
+            <div class="icons">
+              <a href="#"> <i class="fas fa-calendar"></i> 04 Nov, 2022 </a>
+              <a href="#"> <i class="fas fa-calender"></i> by admin </a>
+            </div>
+
+            <h3> Most Common Problems </h3>
+            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. </p>
+            <a href="#" class="btn"> read more </a>
+          </div>
+        </div>
+
+      </div>
+
+    </section>
+    <!-- END Blog -->
+
+
+
+
+
+    <!-- Contact -->
+    <section class="contact" id="contact">
+
+        <h1> Make Appointment </h1>
+
+        <form action="">
+          <span> Your name: </span>
+          <div class="inputBox">
+            <input type="text" palceholder="First Name">
+            <input type="text" palceholder="Last Name">
+          </div>
+
+          <span> Your email: </span>
+          <input type="email" placeholder="Enter your email" class="box">
+
+          <span> Your Phone number: </span>
+          <input type="number" placeholder="Enter your phone number" class="box">
+
+          <span> Appointment date: </span>
+          <input type="datetime-local" class="box">
+          <input type="submit" value="order now" class="btn">
+
+        </form>
+
+    </section>
+    <!-- END Contact -->
+
+
+
+    <!-- Footer -->
+    <section class="footer">
+
+      <div class="box-container">
+          <div class="box">
+            <h3> Address </h3>
+            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod. </p>
+            <div class="share">
+              <a href="#" class="fab fa-facebook"></a>
+              <a href="#" class="fab fa-twitter"></a>
+              <a href="#" class="fab fa-linkedin"></a>
+              <a href="#" class="fab fa-instagram"></a>
+            </div>
+          </div>
+
+          <div class="box">
+            <h3> e-mail </h3>
+            <a href="#" class="link"> health@dental.com </a>
+            <a href="#" class="link"> support@dental.com </a>
+          </div>
+
+          <div class="box">
+            <h3> Call us </h3>
+            <a href="#" class="link"> (305) 632 6644 </a>
+            <a href="#" class="link"> (407) 465 7643 </a>
+          </div>
+
+          <div class="box">
+            <h3> Opening Hours </h3>
+            <p> monday - friday : 8am - 2pm <br/>
+                saturday : CLOSED <br/>
+                sunday : 10am - 3pm
+            </p>
+          </div>
+
+      </div> <!-- ENd Footer Container -->
+
+      <div class="credit">
+        Powered by <span>OtticoStudio</span> | All Rights Reserved
+      </div>
+
+    </section>
+    <!-- END Footer -->
+
+
+
+    <!-- Swiper JS -->
+    <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+    <script src="assets/Swiper-Script.js"></script>
+
+  </body>
+</html>
